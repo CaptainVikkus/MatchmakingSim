21ece7834032e5518dd972b24e9400d87d68ff73
CaptainVikkus <jaketreleaven@gmail.com>
2020-11-22 21:58:41 -0500

    gamelog

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 import socket
 import json
 import random
+import requests
 
 users = [
     "Elli",         #0
@@ -16,27 +17,28 @@ users = [
     ]
 
 def calcExpected(player1, player2):
-    return 1/ (1 + 10^((player2 - player1)/400))
+    return 1/ (1 + 10.0 ** ((player2 - player1)/400))
 
 def calcELO(win: bool, player1, player2, player3):
     score = 0
     kfactor = 100
     if (win):
-        score = (1 + ((calcExpected(player1, player2) + calcExpected(player1, player3))/400)) * kfactor
+        score = (1.0 - ((calcExpected(player1, player2) + calcExpected(player1, player3))/2.0)) * kfactor
     else:
-        score = (0 + ((calcExpected(player1, player2) + calcExpected(player1, player3))/400)) * kfactor
-    return score
+        score = (0.0 - ((calcExpected(player1, player2) + calcExpected(player1, player3))/2)) * kfactor
+    return int(score)
 
 def updateELO(userID, elo):
-    url = "https://k1zg78a86f.execute-api.us-east-2.amazonaws.com/default/returnELOfromID/?" + userID + "&" + elo
-    response = requests.getURL(url)
+    url = "https://9r1e4swuoh.execute-api.us-east-2.amazonaws.com/default/updateELOfromID/?USERID=" + userID + "&ELO=" + str(elo)
+    response = requests.get(url)
+    print(response.content)
     return json.loads(response.content)
 
 
 def buildFight(sock):
     fight = {"Users" : []}
     #build random fighter list between 3 and 10
-    fighters = random.sample(users, random.int(3, 10))
+    fighters = random.sample(users, random.randint(3, 10))
     for fighter in fighters:
         fight["Users"].append(fighter)
     #buildmsg and send to matchmake
@@ -49,23 +51,27 @@ def fight(player1, player2, player3):
     pnum2 = random.randint(0, 100)
     pnum3 = random.randint(0, 100)
 
-    if (pnum1 > pnum2 & pnum1 > pnum3):
-        player1["ELO"] = calcELO(True, int(player1["ELO"]), int(player2["ELO"]), int(player3["ELO"]))
-        player2["ELO"] = calcELO(False, int(player2["ELO"]), int(player1["ELO"]), int(player3["ELO"]))
-        player3["ELO"] = calcELO(False, int(player3["ELO"]), int(player1["ELO"]), int(player2["ELO"]))
+    if (pnum1 > pnum2 and pnum1 > pnum3):
+        player1["ELO"] += calcELO(True, int(player1["ELO"]), int(player2["ELO"]), int(player3["ELO"]))
+        player2["ELO"] += calcELO(False, int(player2["ELO"]), int(player1["ELO"]), int(player3["ELO"]))
+        player3["ELO"] += calcELO(False, int(player3["ELO"]), int(player1["ELO"]), int(player2["ELO"]))
         return player1["UserID"]
-    elif (pnum2 > pnum1 & pnum2 > pnum3):
-        player1["ELO"] = calcELO(False, int(player1["ELO"]), int(player2["ELO"]), int(player3["ELO"]))
-        player2["ELO"] = calcELO(True, int(player2["ELO"]), int(player1["ELO"]), int(player3["ELO"]))
-        player3["ELO"] = calcELO(False, int(player3["ELO"]), int(player1["ELO"]), int(player2["ELO"]))
+    elif (pnum2 > pnum1 and pnum2 > pnum3):
+        player1["ELO"] += calcELO(False, int(player1["ELO"]), int(player2["ELO"]), int(player3["ELO"]))
+        player2["ELO"] += calcELO(True, int(player2["ELO"]), int(player1["ELO"]), int(player3["ELO"]))
+        player3["ELO"] += calcELO(False, int(player3["ELO"]), int(player1["ELO"]), int(player2["ELO"]))
         return player2["UserID"]
-    elif (pnum3 > pnum1 & pnum3 > pnum2):
-        player1["ELO"] = calcELO(False, int(player1["ELO"]), int(player2["ELO"]), int(player3["ELO"]))
-        player2["ELO"] = calcELO(False, int(player2["ELO"]), int(player1["ELO"]), int(player3["ELO"]))
-        player3["ELO"] = calcELO(True, int(player3["ELO"]), int(player1["ELO"]), int(player2["ELO"]))
+    elif (pnum3 > pnum1 and pnum3 > pnum2):
+        player1["ELO"] += calcELO(False, int(player1["ELO"]), int(player2["ELO"]), int(player3["ELO"]))
+        player2["ELO"] += calcELO(False, int(player2["ELO"]), int(player1["ELO"]), int(player3["ELO"]))
+        player3["ELO"] += calcELO(True, int(player3["ELO"]), int(player1["ELO"]), int(player2["ELO"]))
         return player3["UserID"]
     else:
         return "None"
+
+def scribe(file, string):
+    print(string)
+    file.write(string)
 
 
 def main():
@@ -77,29 +83,33 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect((addr, port))
 
+    print(s.getpeername())
+
+    file = open("gamelog.txt", "a")
+    scribe(file, "--- Fights Started ---")
     iFight = 1
     while (numFights > 0):
-        print("Game" + iFight)
+        scribe(file, "Game ID" + str(iFight))
         #send players to lobby
         buildFight(s)
         #get match info
         data, ip = s.recvfrom(1024)
         jdata = json.loads(data)
         if (jdata["GameID"] != -1): #valid game check
-            print("GameID:" + jdata["GameID"])
-            player1 = jdata["Players"]
-            player2 = jdata["Players"]
-            player3 = jdata["Players"]
-            print("Connected Players: ")
-            print(" - " + player1)
-            print(" - " + player2)
-            print(" - " + player3)
+            scribe(file, "GameID:" + str(jdata["GameID"]))
+            player1 = jdata["Players"][0]
+            player2 = jdata["Players"][1]
+            player3 = jdata["Players"][2]
+            scribe(file, "Connected Players: ")
+            scribe(file, " - " + str(player1))
+            scribe(file, " - " + str(player2))
+            scribe(file, " - " + str(player3))
             ##fight
-            print("Winner" + fight(player1,player2,player3))
-            print("Rating Results: ")
-            print(" - " + player1)
-            print(" - " + player2)
-            print(" - " + player3)
+            scribe(file, "Winner " + fight(player1,player2,player3))
+            scribe(file, "Rating Results: ")
+            scribe(file, " - " + str(player1))
+            scribe(file, " - " + str(player2))
+            scribe(file, " - " + str(player3))
             #sendELO for fighters
             updateELO(player1["UserID"], player1["ELO"])
             updateELO(player2["UserID"], player2["ELO"])
@@ -108,9 +118,10 @@ def main():
             numFights -= 1
             iFight += 1
         else:
-            print("Invalid game, trying again")
+            scribe(file, "Invalid game, trying again")
     #finished
-    print("Fights Concluded")
+    scribe(file, "--- Fights Concluded ---")
+    file.close()
 
 
 if __name__ == '__main__':
